4efb0aafd3672a5d341c357edfe48df0891e224a
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-08-22 18:43:40 +0200

    .

--- src/ConfigPopup.js
+++ src/ConfigPopup.js
@@ -9,12 +9,12 @@ export class ConfigPopup extends HTMLElement {
         super();
         this.append(template({
             t,
-            categories: Object.entries(objectsList.columns.reduce((o,c) => {
-                 o[c.category] = (o[c.category] ?? [])
+            categories: Object.entries(objectsList.columns.reduce((o, c) => {
+                o[c.category] = (o[c.category] ?? [])
                 o[c.category].push(c);
-                 return o;
+                return o;
             }, {}))
-                .map(([category, columns]) => ({name: category ?? '', columns}))
+                .map(([category, columns]) => ({name: category=='undefined'?'':category, columns}))
         }))
         this.querySelector('.mode').onchange = () => {
             objectsList.infiniteScrollEnabled = this.querySelector('.mode').value == 'scrollMode'
@@ -28,8 +28,8 @@ export class ConfigPopup extends HTMLElement {
             objectsList.refresh()
         }
         this.addEventListener('blur', () => {
-            if(!this.matches(':focus-within, :focus'))
-            this.remove();
+            if (!this.matches(':focus-within, :focus'))
+                this.remove();
         })
         this.querySelector('select').focus()
         console.log('ssss')
@@ -46,24 +46,39 @@ export class ConfigPopup extends HTMLElement {
         }
 
         for (const filterContainer of this.querySelectorAll('table .filter')) {
-            const filter=new ColumnFilter();
+            const filter = new ColumnFilter();
             filterContainer.append(filter)
             filter.addEventListener('x-filter', (e) => {
-                console.log('dddd',e)
+                console.log('dddd', e)
                 objectsList.columnFilters.set(filterContainer.dataset.name, e.detail)
                 objectsList.refresh()
             });
         }
+
+        this.querySelector('[type="search"]').oninput = (event) => {
+            for (const table of this.querySelectorAll('.categoryTableContainer')) {
+                let anyVisible = false;
+                for (const row of table.querySelectorAll('tbody tr')) {
+                    const visible = row.children[1].textContent.toLowerCase().includes(event.target.value.toLowerCase())
+                    row.style.display = visible ? '' : 'none'
+                    if (visible)
+                        anyVisible = true;
+                }
+                table.style.display = anyVisible ? '' : 'none'
+            }
+        }
+
         this.tabIndex = 0
-        setTimeout(()=>this.focus(), 1)
+        setTimeout(() => this.focus(), 1)
         setTimeout(() => this.checkFocus(), 100)
     }
+
     checkFocus() {
         console.log('checkFocus')
         if (!this.matches(':focus-within, :focus'))
             this.remove();
 
-        if(this.parentNode)
+        if (this.parentNode)
             setTimeout(() => this.checkFocus(), 100)
     }
 }
